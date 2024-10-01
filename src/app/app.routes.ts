b102f09c2284e44ea60190093534091fd79971df
Eric-Obeng <obengeric.3958@gmail.com>
2024-10-01 00:11:16 +0000

    configure routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./components/auth/register/register.component').then(
+        (r) => r.RegisterComponent
+      ),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./components/auth/login/login.component').then(
+        (l) => l.LoginComponent
+      ),
+  },
+  {
+    path: 'profile',
+    loadComponent: () =>
+      import('./components/auth/profile/profile.component').then(
+        (p) => p.ProfileComponent
+      ),
+  },
+];
